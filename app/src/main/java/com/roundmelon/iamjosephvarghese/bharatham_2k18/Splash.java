463081af39533010ad9db3871cc795c954e4888b
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-10-11 23:08:57 +0530

    Initial Commit

---
+++ app/src/main/java/com/roundmelon/iamjosephvarghese/bharatham_2k18/Splash.java
@@ -0,0 +1,13 @@
+package com.roundmelon.iamjosephvarghese.bharatham_2k18;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
