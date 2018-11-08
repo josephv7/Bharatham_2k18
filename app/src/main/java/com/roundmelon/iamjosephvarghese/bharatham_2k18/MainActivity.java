6806ad7f40077991c3e62cd4662a9137a37b9efd
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-11-08 10:09:48 +0530

    chamges

--- app/src/main/java/com/roundmelon/iamjosephvarghese/bharatham_2k18/MainActivity.java
+++ app/src/main/java/com/roundmelon/iamjosephvarghese/bharatham_2k18/MainActivity.java
@@ -4,8 +4,7 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
