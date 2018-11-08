a8498a962d19c13bf1315c123b1e0b4e85ab1461
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-11-08 12:45:41 +0530

    showing gallery only

--- app/src/main/java/com/roundmelon/iamjosephvarghese/bharatham_2k18/MainActivity.java
+++ app/src/main/java/com/roundmelon/iamjosephvarghese/bharatham_2k18/MainActivity.java
@@ -69,7 +69,7 @@ public class MainActivity extends AppCompatActivity
         gallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,SelfieGallery.class);
+                Intent intent = new Intent(MainActivity.this,Gallery.class);
                 startActivity(intent);
             }
         });
